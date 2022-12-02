4b4c698b275343f5a3b4f0a64ede30806c780dc0
Aleksandr Kirillov <32141102+axkirillov@users.noreply.github.com>
2022-12-02 09:53:54 +0100

    first commit

---
+++ config.fish
@@ -0,0 +1,16 @@
+source $HOME/fish/globals.fish
+source $HOME/fish/aliases.fish
+source $HOME/fish/bindings.fish
+
+# functions are in $HOME/fish/
+for file in $HOME/fish/functions/*.fish
+	source $file
+end
+
+# initializations go at the end:
+# prompt 
+starship init fish | source
+# rbenv
+status --is-interactive; and rbenv init - fish | source
+
+set PATH (go env GOPATH)/bin:$PATH
